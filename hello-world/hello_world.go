e8c3d38531e6209e717819225f704ed0b8fd1b05
Didier Domínguez <dadu0699@hotmail.com>
2022-11-25 16:11:10 -0600

    feat: Hello World

---
+++ hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting offers an opening greeting.
+package greeting
+
+// HelloWorld returns the greeting "Hello world!".
+func HelloWorld() string {
+	return "Hello, World!"
+}
---
+++ hello-world/hello_world_test.go
@@ -0,0 +1,24 @@
+package greeting
+
+import "testing"
+
+// Define a function named HelloWorld that takes no arguments,
+// and returns a string.
+// In other words, define a function with the following signature:
+// HelloWorld() string
+
+func TestHelloWorld(t *testing.T) {
+	expected := "Hello, World!"
+	if observed := HelloWorld(); observed != expected {
+		t.Fatalf("HelloWorld() = %v, want %v", observed, expected)
+	}
+}
+
+func BenchmarkHelloWorld(b *testing.B) {
+	if testing.Short() {
+		b.Skip("skipping benchmark in short mode.")
+	}
+	for i := 0; i < b.N; i++ {
+		HelloWorld()
+	}
+}
